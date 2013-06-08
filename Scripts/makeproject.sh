aa7b7d66660956e4d6041579ab8ae17bf9c8c31b
Sam Deane <sam@elegantchaos.com>
2013-06-08 12:32:47 +0100

    Tweaked some settings.

--- Scripts/makeproject.sh
+++ Scripts/makeproject.sh
@@ -5,6 +5,6 @@ name=$2
 
 mkdir -p "$dest/$name"
 cp -R . "$dest/$name/"
-grep -lr -e '{{name}}' "$dest/$name" | xargs sed -i "'s/{{name}}/$name/g'"
+perl -p -i -e "s/{{name}}/$name/g" "$dest/$name/"*
 #grep -lr -e '{{name}}' "$dest/$name" | xargs sed -i "'s/{{name}}/$name/g'"
 open "$dest/$name"
